3596b5a4b2646fbd9315c88629929b41f2c43da1
Maxim <m.krylov88@mail.ru>
2021-01-27 16:51:25 +0300

    Create class Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,21 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        boolean result = bean == bean1;
+        System.out.println(result);
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getName());
+
+        boolean resultCat = catBean == catBean1;
+        System.out.println(resultCat);
     }
 }
